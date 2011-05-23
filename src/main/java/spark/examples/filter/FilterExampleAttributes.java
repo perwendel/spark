71278e93538b17944bdcbbad205944a1a19d1d91
Per Wendel <per.wendel@sonyericsson.com>
2011-05-23 13:14:57 +0200

    added example to README

--- src/main/java/spark/examples/filter/FilterExampleAttributes.java
+++ src/main/java/spark/examples/filter/FilterExampleAttributes.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2011- Per Wendel
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package spark.examples.filter;
 
 import static spark.Spark.after;
@@ -7,6 +23,11 @@ import spark.Request;
 import spark.Response;
 import spark.Route;
 
+/**
+ * Example showing the use of attributes
+ *
+ * @author Per Wendel
+ */
 public class FilterExampleAttributes {
 
     public static void main(String[] args) {
