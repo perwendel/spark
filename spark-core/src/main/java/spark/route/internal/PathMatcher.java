e0b2e013b3453c9d79e978cc19a4d796b61eb9a6
Matthias Hryniszak <padcom@gmail.com>
2011-07-01 00:57:35 +0200

    Refactored PathMatcher to be a lot smaller and easier.

--- spark-core/src/main/java/spark/route/internal/PathMatcher.java
+++ spark-core/src/main/java/spark/route/internal/PathMatcher.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2011- Per Wendel, Matthias Hryniszak
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package spark.route.internal;
 
 import java.util.List;
@@ -5,75 +21,25 @@ import java.util.List;
 import spark.utils.SparkUtils;
 
 public class PathMatcher {
-	public boolean match(String template, String path2) {
-		if (!template.endsWith("*") 
-				&& ((path2.endsWith("/") && !template.endsWith("/")) || (template.endsWith("/") && !path2.endsWith("/")))) {
-			// One and not both ends with slash
-			return false;
+	public boolean match(String template, String path) {
+		List<String> templateItems = SparkUtils.convertRouteToList(template);
+		List<String> pathItems = SparkUtils.convertRouteToList(path);
+		int count = Math.min(templateItems.size(), pathItems.size());
+		for (int i = 0; i < count; i++) {
+			if (templateItems.get(i).equals("*")) 
+				return true;
+			if (templateItems.get(i).startsWith(":")) 
+				continue;
+			String purePath = extractPurePath(pathItems.get(i));
+			if (!templateItems.get(i).equals(purePath))
+				return false;
 		}
-		if (template.equals(path2)) {
-			// path2s are the same
-			return true;
-		}
-
-		// check params
-		List<String> thisPath2List = SparkUtils.convertRouteToList(template);
-		List<String> path2List = SparkUtils.convertRouteToList(path2);
-
-		int thispath2Size = thisPath2List.size();
-		int path2Size = path2List.size();
-
-		if (thispath2Size == path2Size) {
-			for (int i = 0; i < thispath2Size; i++) {
-				String thispath2Part = thisPath2List.get(i);
-				String path2Part = path2List.get(i);
-
-				if ((i == thispath2Size - 1)) {
-					if (thispath2Part.equals("*") && template.endsWith("*")) {
-						// wildcard match
-						return true;
-					}
-				}
-
-				if (!thispath2Part.startsWith(":")
-						&& !thispath2Part.equals(path2Part)) {
-					return false;
-				}
-			}
-			// All parts matched
-			return true;
-		} else {
-			// Number of "path2 parts" not the same
-			// check wild card:
-			if (template.endsWith("*")) {
-				if (path2Size == (thispath2Size - 1) && (path2.endsWith("/"))) {
-					// Hack for making wildcards work with trailing slash
-					path2List.add("");
-					path2List.add("");
-					path2Size += 2;
-				}
-
-				if (thispath2Size < path2Size) {
-					for (int i = 0; i < thispath2Size; i++) {
-						String thispath2Part = thisPath2List.get(i);
-						String path2Part = path2List.get(i);
-						if (thispath2Part.equals("*")
-								&& (i == thispath2Size - 1)
-								&& template.endsWith("*")) {
-							// wildcard match
-							return true;
-						}
-						if (!thispath2Part.startsWith(":")
-								&& !thispath2Part.equals(path2Part)) {
-							return false;
-						}
-					}
-					// All parts matched
-					return true;
-				}
-				// End check wild card
-			}
-			return false;
-		}
+		return templateItems.size() == pathItems.size();
+	}
+	
+	private String extractPurePath(String path) {
+		int delimiterIndex = path.indexOf('?');
+		if (delimiterIndex == -1) return path;
+		return path.substring(0, delimiterIndex - 1);
 	}
 }
--- spark-core/src/test/java/spark/route/internal/PathMatcherTest.java
+++ spark-core/src/test/java/spark/route/internal/PathMatcherTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2011- Per Wendel, Matthias Hryniszak
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package spark.route.internal;
 
 import org.junit.Assert;
@@ -10,10 +26,40 @@ public class PathMatcherTest {
 	@Test
 	public void willReturnTrueIfTemplateAndRouteAreEqual() {
 		Assert.assertTrue(matcher.match("/test1/test2", "/test1/test2"));
+		Assert.assertFalse(matcher.match("/test1/test2", "/test1/test2?param=value"));
 	}
 	
 	@Test
-	public void willReturnTrueIfTemplateHasNoWildcardsAndRouteIsNotEqual() {
+	public void willReturnFalseIfTemplateHasNoWildcardsAndRouteIsNotEqual() {
+		Assert.assertFalse(matcher.match("/test1", "/test2/test3"));
+		Assert.assertFalse(matcher.match("/test1", "/test2/test3?param=value"));
 		Assert.assertFalse(matcher.match("/test1/test2", "/test3/test4"));
+		Assert.assertFalse(matcher.match("/test1/test2", "/test3/test4?param=value"));
+	}
+	
+	@Test
+	public void willReturnTrueIfTemplateHasWildcardsAndRouteMatches() {
+		Assert.assertTrue(matcher.match("/test1/*", "/test1/test3"));
+		Assert.assertTrue(matcher.match("/test1/*", "/test1/test3?param=value"));
+		Assert.assertTrue(matcher.match("/test1/*", "/test1/test3/test4"));
+		Assert.assertTrue(matcher.match("/test1/*", "/test1/test3/test4?param=value"));
+	}
+	
+	@Test
+	public void willReturnFalseIfTemplateHasWildcardsAndRouteDoesNotMatch() {
+		Assert.assertFalse(matcher.match("/test1/*", "/test2/test3"));
+		Assert.assertFalse(matcher.match("/test1/*", "/test2/test3?param=value"));
+	}
+	
+	@Test
+	public void willReturnTrueIfTemplateHasParameters() {
+		Assert.assertTrue(matcher.match("/test1/:id", "/test1/1"));
+		Assert.assertTrue(matcher.match("/test1/:param1/:param2", "/test1/1/2"));
+	}
+	
+	@Test
+	public void willReturnTrueIfTemplateHasFewerParametersThanThePath() {
+		Assert.assertFalse(matcher.match("/test1/:id", "/test1/1/2"));
+		Assert.assertFalse(matcher.match("/test1/:param1/:param2", "/test1/1/2/3"));
 	}
 }
