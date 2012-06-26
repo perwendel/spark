3b53b3c114809dc9fb88a4b0011e18e6a410009e
Andrew Eells <aeells@qmetric.co.uk>
2012-06-26 11:38:47 +0100

    Changing to production ELB's.

--- metrics/src/main/static/js/metrics-app.js
+++ metrics/src/main/static/js/metrics-app.js
@@ -1,11 +1,12 @@
-window.addEvent('domready', function() {
-	new Metrics(document.id('occupation-service'), {
-		url: 'http://localhost:4600/metrics',
-		requestDelay: 5000,
-		shiftAfter: 50
-	});
-	
-	new Metrics(document.id('enquiry-search-service'), {
-		url: 'http://localhost:4610/metrics'
-	});
+window.addEvent('domready', function ()
+{
+    new Metrics(document.id('occupation-service'), {
+        url:'http://prod-occupation-service-1522070611.eu-west-1.elb.amazonaws.com/metrics',
+        requestDelay:5000,
+        shiftAfter:50
+    });
+
+    new Metrics(document.id('enquiry-search-service'), {
+        url:'http://prod-enquiry-search-service-1387825206.eu-west-1.elb.amazonaws.com/metrics'
+    });
 });
