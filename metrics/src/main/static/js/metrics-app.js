66f98c76d0db31f736d979a7921a78a6c10b2c5d
wayne <wbuckley@qmetric.co.uk>
2012-06-26 11:52:05 +0100

    add job service at production to graphs

--- metrics/src/main/static/js/metrics-app.js
+++ metrics/src/main/static/js/metrics-app.js
@@ -9,4 +9,8 @@ window.addEvent('domready', function ()
     new Metrics(document.id('enquiry-search-service'), {
         url:'http://prod-enquiry-search-service-1387825206.eu-west-1.elb.amazonaws.com/metrics'
     });
+
+    new Metrics(document.id('job-service'), {
+        url:'http://prod-job-service-1458688668.eu-west-1.elb.amazonaws.com/metrics'
+    });
 });
