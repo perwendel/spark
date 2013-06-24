77d35e809589a93707ac48bc03c47db5076c24b5
Wendel Per <wendel.per@sonymobile.com>
2013-06-24 13:09:18 +0200

    changed to just request URI

--- src/main/java/spark/webserver/MatcherFilter.java
+++ src/main/java/spark/webserver/MatcherFilter.java
@@ -4,7 +4,7 @@
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- *  
+ *
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -40,7 +40,7 @@ import spark.route.RouteMatcher;
 
 /**
  * Filter for matching of filters and routes.
- * 
+ *
  * @author Per Wendel
  */
 public class MatcherFilter implements Filter {
@@ -55,7 +55,7 @@ public class MatcherFilter implements Filter {
 
     /**
      * Constructor
-     * 
+     *
      * @param routeMatcher The route matcher
      * @param isServletContext If true, chain.doFilter will be invoked if request is not consumed by Spark.
      * @param hasOtherHandlers If true, do nothing if request is not consumed by Spark in order to let others handlers process the request.
@@ -79,17 +79,17 @@ public class MatcherFilter implements Filter {
         String httpMethodStr = httpRequest.getMethod().toLowerCase(); // NOSONAR
         String uri = httpRequest.getRequestURI(); // NOSONAR
         String acceptType = httpRequest.getHeader(ACCEPT_TYPE_REQUEST_MIME_HEADER);
-        
+
         String bodyContent = null;
 
         RequestWrapper req = new RequestWrapper();
         ResponseWrapper res = new ResponseWrapper();
-        
+
         LOG.debug("httpMethod:" + httpMethodStr + ", uri: " + uri);
         try {
             // BEFORE filters
             List<RouteMatch> matchSet = routeMatcher.findTargetsForRequestedRoute(HttpMethod.before, uri, acceptType);
-            
+
             for (RouteMatch filterMatch : matchSet) {
                 Object filterTarget = filterMatch.getTarget();
                 if (filterTarget instanceof spark.Filter) {
@@ -100,22 +100,22 @@ public class MatcherFilter implements Filter {
 
                     req.setDelegate(request);
                     res.setDelegate(response);
-                    
+
                     filter.handle(req, res);
 
                     String bodyAfterFilter = Access.getBody(response);
                     if (bodyAfterFilter != null) {
                         bodyContent = bodyAfterFilter;
                     }
-                }   
+                }
             }
             // BEFORE filters, END
-            
+
             HttpMethod httpMethod = HttpMethod.valueOf(httpMethodStr);
-            
+
             RouteMatch match = null;
             match = routeMatcher.findTargetForRequestedRoute(httpMethod, uri, acceptType);
-            
+
             Object target = null;
             if (match != null) {
                 target = match.getTarget();
@@ -131,10 +131,10 @@ public class MatcherFilter implements Filter {
                         Route route = ((Route) target);
                         Request request = RequestResponseFactory.create(match, httpRequest);
                         Response response = RequestResponseFactory.create(httpResponse);
-                        
+
                         req.setDelegate(request);
                         res.setDelegate(response);
-                        
+
                         Object element = route.handle(req, res);
                         result = route.render(element);
                     }
@@ -154,16 +154,16 @@ public class MatcherFilter implements Filter {
 
             // AFTER filters
             matchSet = routeMatcher.findTargetsForRequestedRoute(HttpMethod.after, uri, acceptType);
-            
+
             for (RouteMatch filterMatch : matchSet) {
                 Object filterTarget = filterMatch.getTarget();
                 if (filterTarget instanceof spark.Filter) {
                     Request request = RequestResponseFactory.create(filterMatch, httpRequest);
                     Response response = RequestResponseFactory.create(httpResponse);
-                    
+
                     req.setDelegate(request);
                     res.setDelegate(response);
-                    
+
                     spark.Filter filter = (spark.Filter) filterTarget;
                     filter.handle(req, res);
 
@@ -171,10 +171,10 @@ public class MatcherFilter implements Filter {
                     if (bodyAfterFilter != null) {
                         bodyContent = bodyAfterFilter;
                     }
-                }   
+                }
             }
             // AFTER filters, END
-            
+
         } catch (HaltException hEx) {
             LOG.debug("halt performed");
             httpResponse.setStatus(hEx.getStatusCode());
@@ -186,14 +186,14 @@ public class MatcherFilter implements Filter {
         }
 
         boolean consumed = bodyContent != null;
-        
+
         if (!consumed && hasOtherHandlers) {
             throw new NotConsumedException();
         }
-        
+
         if (!consumed && !isServletContext) {
             httpResponse.setStatus(HttpServletResponse.SC_NOT_FOUND);
-            bodyContent = String.format(NOT_FOUND, httpRequest.getRequestURL().toString());
+            bodyContent = String.format(NOT_FOUND, uri);
             consumed = true;
         }
 
