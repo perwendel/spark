8b901b146a139c8e59983d81c1d4574ac7bc9ca5
xorrr <derxor@gmail.com>
2014-05-30 18:23:28 +0200

    refactor doMethod

--- src/test/java/spark/BooksIntegrationTest.java
+++ src/test/java/spark/BooksIntegrationTest.java
@@ -9,6 +9,8 @@ import static spark.Spark.before;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.util.List;
 import java.util.Map;
@@ -180,22 +182,24 @@ public class BooksIntegrationTest {
         UrlResponse response = new UrlResponse();
 
         try {
-            URL url = new URL("http://localhost:" + PORT + path);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod(requestMethod);
-
-            connection.connect();
-
-            String res = IOUtils.toString(connection.getInputStream());
-
-            response.body = res;
-            response.status = connection.getResponseCode();
-            response.headers = connection.getHeaderFields();
+            getResponse(requestMethod, path, response);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         return response;
+    }
+
+    private static void getResponse(String requestMethod, String path, UrlResponse response)
+            throws MalformedURLException, IOException, ProtocolException {
+        URL url = new URL("http://localhost:" + PORT + path);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod(requestMethod);
+        connection.connect();
+        String res = IOUtils.toString(connection.getInputStream());
+        response.body = res;
+        response.status = connection.getResponseCode();
+        response.headers = connection.getHeaderFields();
     }
 
     private static class UrlResponse {
