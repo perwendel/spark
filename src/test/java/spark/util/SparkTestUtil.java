e128605de2f008ea01431be6cde0e263704351d6
Carson Gross <carsongross@gmail.com>
2014-07-08 13:08:51 -0700

    Really fix spacing

--- src/test/java/spark/util/SparkTestUtil.java
+++ src/test/java/spark/util/SparkTestUtil.java
@@ -36,46 +36,46 @@ import org.apache.http.util.EntityUtils;
 
 public class SparkTestUtil {
 
-	private int port;
+    private int port;
 
-	private HttpClient httpClient;
+    private HttpClient httpClient;
 
-	public SparkTestUtil(int port) {
-		this.port = port;
-		Scheme http = new Scheme("http", port, PlainSocketFactory.getSocketFactory());
-		Scheme https = new Scheme("https", port, new org.apache.http.conn.ssl.SSLSocketFactory(getSslFactory(), null));
-		SchemeRegistry sr = new SchemeRegistry();
-		sr.register(http);
-		sr.register(https);
-		ClientConnectionManager connMrg = new BasicClientConnectionManager(sr);
-		this.httpClient = new DefaultHttpClient(connMrg);
-	}
+    public SparkTestUtil(int port) {
+        this.port = port;
+        Scheme http = new Scheme("http", port, PlainSocketFactory.getSocketFactory());
+        Scheme https = new Scheme("https", port, new org.apache.http.conn.ssl.SSLSocketFactory(getSslFactory(), null));
+        SchemeRegistry sr = new SchemeRegistry();
+        sr.register(http);
+        sr.register(https);
+        ClientConnectionManager connMrg = new BasicClientConnectionManager(sr);
+        this.httpClient = new DefaultHttpClient(connMrg);
+    }
 
-	public UrlResponse doMethodSecure(String requestMethod, String path, String body)
-			throws Exception {
-		return doMethod(requestMethod, path, body, true, "text/html");
-	}
+    public UrlResponse doMethodSecure(String requestMethod, String path, String body)
+            throws Exception {
+        return doMethod(requestMethod, path, body, true, "text/html");
+    }
 
-	public UrlResponse doMethod(String requestMethod, String path, String body) throws Exception {
-		return doMethod(requestMethod, path, body, false, "text/html");
-	}
+    public UrlResponse doMethod(String requestMethod, String path, String body) throws Exception {
+        return doMethod(requestMethod, path, body, false, "text/html");
+    }
 
-	public UrlResponse doMethodSecure(String requestMethod, String path, String body, String acceptType)
-			throws Exception {
-		return doMethod(requestMethod, path, body, true, acceptType);
-	}
+    public UrlResponse doMethodSecure(String requestMethod, String path, String body, String acceptType)
+            throws Exception {
+        return doMethod(requestMethod, path, body, true, acceptType);
+    }
 
-	public UrlResponse doMethod(String requestMethod, String path, String body, String acceptType) throws Exception {
-		return doMethod(requestMethod, path, body, false, acceptType);
-	}
+    public UrlResponse doMethod(String requestMethod, String path, String body, String acceptType) throws Exception {
+        return doMethod(requestMethod, path, body, false, acceptType);
+    }
 
-	private UrlResponse doMethod(String requestMethod, String path, String body, boolean secureConnection,
-			String acceptType) throws Exception {
+    private UrlResponse doMethod(String requestMethod, String path, String body, boolean secureConnection,
+                                 String acceptType) throws Exception {
         return doMethod(requestMethod, path, body, secureConnection, acceptType, null);
-	}
+    }
 
     public UrlResponse doMethod(String requestMethod, String path, String body, boolean secureConnection,
-                                 String acceptType, Map<String, String> reqHeaders) throws IOException {
+                                String acceptType, Map<String, String> reqHeaders) throws IOException {
         HttpUriRequest httpRequest = getHttpRequest(requestMethod, path, body, secureConnection, acceptType, reqHeaders);
         HttpResponse httpResponse = httpClient.execute(httpRequest);
 
@@ -98,174 +98,173 @@ public class SparkTestUtil {
 
     private HttpUriRequest getHttpRequest(String requestMethod, String path, String body, boolean secureConnection,
                                           String acceptType, Map<String, String> reqHeaders) {
-		try {
-			String protocol = secureConnection ? "https" : "http";
-			String uri = protocol + "://localhost:" + port + path;
+        try {
+            String protocol = secureConnection ? "https" : "http";
+            String uri = protocol + "://localhost:" + port + path;
 
-			if (requestMethod.equals("GET")) {
-				HttpGet httpGet = new HttpGet(uri);
-				httpGet.setHeader("Accept", acceptType);
-        addHeaders(reqHeaders, httpGet);
-				return httpGet;
-			}
+            if (requestMethod.equals("GET")) {
+                HttpGet httpGet = new HttpGet(uri);
+                httpGet.setHeader("Accept", acceptType);
+                addHeaders(reqHeaders, httpGet);
+                return httpGet;
+            }
 
-			if (requestMethod.equals("POST")) {
-				HttpPost httpPost = new HttpPost(uri);
-				httpPost.setHeader("Accept", acceptType);
-        addHeaders(reqHeaders, httpPost);
-				httpPost.setEntity(new StringEntity(body));
-				return httpPost;
-			}
+            if (requestMethod.equals("POST")) {
+                HttpPost httpPost = new HttpPost(uri);
+                httpPost.setHeader("Accept", acceptType);
+                addHeaders(reqHeaders, httpPost);
+                httpPost.setEntity(new StringEntity(body));
+                return httpPost;
+            }
 
-			if (requestMethod.equals("PATCH")) {
-				HttpPatch httpPatch = new HttpPatch(uri);
-				httpPatch.setHeader("Accept", acceptType);
-        addHeaders(reqHeaders, httpPatch);
-				httpPatch.setEntity(new StringEntity(body));
-				return httpPatch;
-			}
+            if (requestMethod.equals("PATCH")) {
+                HttpPatch httpPatch = new HttpPatch(uri);
+                httpPatch.setHeader("Accept", acceptType);
+                addHeaders(reqHeaders, httpPatch);
+                httpPatch.setEntity(new StringEntity(body));
+                return httpPatch;
+            }
 
-			if (requestMethod.equals("DELETE")) {
-				HttpDelete httpDelete = new HttpDelete(uri);
-        addHeaders(reqHeaders, httpDelete);
-				httpDelete.setHeader("Accept", acceptType);
-				return httpDelete;
-			}
+            if (requestMethod.equals("DELETE")) {
+                HttpDelete httpDelete = new HttpDelete(uri);
+                addHeaders(reqHeaders, httpDelete);
+                httpDelete.setHeader("Accept", acceptType);
+                return httpDelete;
+            }
 
-			if (requestMethod.equals("PUT")) {
-				HttpPut httpPut = new HttpPut(uri);
-				httpPut.setHeader("Accept", acceptType);
-        addHeaders(reqHeaders, httpPut);
-				httpPut.setEntity(new StringEntity(body));
-				return httpPut;
-			}
+            if (requestMethod.equals("PUT")) {
+                HttpPut httpPut = new HttpPut(uri);
+                httpPut.setHeader("Accept", acceptType);
+                addHeaders(reqHeaders, httpPut);
+                httpPut.setEntity(new StringEntity(body));
+                return httpPut;
+            }
 
-      if (requestMethod.equals("HEAD")) {
-        HttpHead httpHead = new HttpHead(uri);
-        addHeaders(reqHeaders, httpHead);
-        return httpHead;
-      }
+            if (requestMethod.equals("HEAD")) {
+                HttpHead httpHead = new HttpHead(uri);
+                addHeaders(reqHeaders, httpHead);
+                return httpHead;
+            }
 
-      if (requestMethod.equals("TRACE")) {
-        HttpTrace httpTrace = new HttpTrace(uri);
-        addHeaders(reqHeaders, httpTrace);
-        return httpTrace;
-      }
+            if (requestMethod.equals("TRACE")) {
+                HttpTrace httpTrace = new HttpTrace(uri);
+                addHeaders(reqHeaders, httpTrace);
+                return httpTrace;
+            }
 
-      if (requestMethod.equals("OPTIONS")) {
-        HttpOptions httpOptions = new HttpOptions(uri);
-        addHeaders(reqHeaders, httpOptions);
-        return httpOptions;
-			}
+            if (requestMethod.equals("OPTIONS")) {
+                HttpOptions httpOptions = new HttpOptions(uri);
+                addHeaders(reqHeaders, httpOptions);
+                return httpOptions;
+            }
 
-			throw new IllegalArgumentException("Unknown method " + requestMethod);
+            throw new IllegalArgumentException("Unknown method " + requestMethod);
 
-		} catch (UnsupportedEncodingException e) {
-			throw new RuntimeException(e);
-		}
-
-	}
-
-  private void addHeaders(Map<String, String> reqHeaders, HttpRequest req) {
-    if (reqHeaders != null) {
-      for (Map.Entry<String, String> header : reqHeaders.entrySet()) {
-        req.addHeader(header.getKey(), header.getValue());
-      }
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
     }
-  }
+
+    private void addHeaders(Map<String, String> reqHeaders, HttpRequest req) {
+        if (reqHeaders != null) {
+            for (Map.Entry<String, String> header : reqHeaders.entrySet()) {
+                req.addHeader(header.getKey(), header.getValue());
+            }
+        }
+    }
 
     public int getPort() {
-		return port;
-	}
+        return port;
+    }
 
-	/**
-	 * Convenience method to use own truststore on SSL Sockets. Will default to
-	 * the self signed keystore provided in resources, but will respect
-	 * <p/>
-	 * -Djavax.net.ssl.keyStore=serverKeys
-	 * -Djavax.net.ssl.keyStorePassword=password
-	 * -Djavax.net.ssl.trustStore=serverTrust
-	 * -Djavax.net.ssl.trustStorePassword=password SSLApplication
-	 * <p/>
-	 * So these can be used to specify other key/trust stores if required.
-	 * 
-	 * @return an SSL Socket Factory using either provided keystore OR the
-	 *         keystore specified in JVM params
-	 */
-	private SSLSocketFactory getSslFactory() {
-		KeyStore keyStore = null;
-		try {
-			keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
-			FileInputStream fis = new FileInputStream(getTrustStoreLocation());
-			keyStore.load(fis, getTrustStorePassword().toCharArray());
-			fis.close();
+    /**
+     * Convenience method to use own truststore on SSL Sockets. Will default to
+     * the self signed keystore provided in resources, but will respect
+     * <p/>
+     * -Djavax.net.ssl.keyStore=serverKeys
+     * -Djavax.net.ssl.keyStorePassword=password
+     * -Djavax.net.ssl.trustStore=serverTrust
+     * -Djavax.net.ssl.trustStorePassword=password SSLApplication
+     * <p/>
+     * So these can be used to specify other key/trust stores if required.
+     *
+     * @return an SSL Socket Factory using either provided keystore OR the
+     *         keystore specified in JVM params
+     */
+    private SSLSocketFactory getSslFactory() {
+        KeyStore keyStore = null;
+        try {
+            keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            FileInputStream fis = new FileInputStream(getTrustStoreLocation());
+            keyStore.load(fis, getTrustStorePassword().toCharArray());
+            fis.close();
 
-			TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-			tmf.init(keyStore);
-			SSLContext ctx = SSLContext.getInstance("TLS");
-			ctx.init(null, tmf.getTrustManagers(), null);
-			return ctx.getSocketFactory();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            tmf.init(keyStore);
+            SSLContext ctx = SSLContext.getInstance("TLS");
+            ctx.init(null, tmf.getTrustManagers(), null);
+            return ctx.getSocketFactory();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	/**
-	 * Return JVM param set keystore or default if not set.
-	 * 
-	 * @return Keystore location as string
-	 */
-	public static String getKeyStoreLocation() {
-		String keyStoreLoc = System.getProperty("javax.net.ssl.keyStore");
-		return keyStoreLoc == null ? "./src/test/resources/keystore.jks" : keyStoreLoc;
-	}
+    /**
+     * Return JVM param set keystore or default if not set.
+     *
+     * @return Keystore location as string
+     */
+    public static String getKeyStoreLocation() {
+        String keyStoreLoc = System.getProperty("javax.net.ssl.keyStore");
+        return keyStoreLoc == null ? "./src/test/resources/keystore.jks" : keyStoreLoc;
+    }
 
-	/**
-	 * Return JVM param set keystore password or default if not set.
-	 * 
-	 * @return Keystore password as string
-	 */
-	public static String getKeystorePassword() {
-		String password = System.getProperty("javax.net.ssl.keyStorePassword");
-		return password == null ? "password" : password;
-	}
+    /**
+     * Return JVM param set keystore password or default if not set.
+     *
+     * @return Keystore password as string
+     */
+    public static String getKeystorePassword() {
+        String password = System.getProperty("javax.net.ssl.keyStorePassword");
+        return password == null ? "password" : password;
+    }
 
-	/**
-	 * Return JVM param set truststore location, or keystore location if not
-	 * set. if keystore not set either, returns default
-	 * 
-	 * @return truststore location as string
-	 */
-	public static String getTrustStoreLocation() {
-		String trustStoreLoc = System.getProperty("javax.net.ssl.trustStore");
-		return trustStoreLoc == null ? getKeyStoreLocation() : trustStoreLoc;
-	}
+    /**
+     * Return JVM param set truststore location, or keystore location if not
+     * set. if keystore not set either, returns default
+     *
+     * @return truststore location as string
+     */
+    public static String getTrustStoreLocation() {
+        String trustStoreLoc = System.getProperty("javax.net.ssl.trustStore");
+        return trustStoreLoc == null ? getKeyStoreLocation() : trustStoreLoc;
+    }
 
-	/**
-	 * Return JVM param set truststore password or keystore password if not set.
-	 * If still not set, will return default password
-	 * 
-	 * @return truststore password as string
-	 */
-	public static String getTrustStorePassword() {
-		String password = System.getProperty("javax.net.ssl.trustStorePassword");
-		return password == null ? getKeystorePassword() : password;
-	}
+    /**
+     * Return JVM param set truststore password or keystore password if not set.
+     * If still not set, will return default password
+     *
+     * @return truststore password as string
+     */
+    public static String getTrustStorePassword() {
+        String password = System.getProperty("javax.net.ssl.trustStorePassword");
+        return password == null ? getKeystorePassword() : password;
+    }
 
-	public static class UrlResponse {
+    public static class UrlResponse {
 
-		public Map<String, String> headers;
-		public String body;
-		public int status;
-	}
+        public Map<String, String> headers;
+        public String body;
+        public int status;
+    }
 
-	public static void sleep(long time) {
-		try {
-			Thread.sleep(time);
-		} catch (Exception e) {
-		}
-	}
+    public static void sleep(long time) {
+        try {
+            Thread.sleep(time);
+        } catch (Exception e) {
+        }
+    }
 
 }
