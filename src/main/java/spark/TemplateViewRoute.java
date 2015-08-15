0eb3874dfdc824ab4bc2a288d300861e7226464f
Per Wendel <per.i.wendel@gmail.com>
2015-08-15 20:42:33 +0200

    added missing javadoc required by Sonatype

--- src/main/java/spark/TemplateViewRoute.java
+++ src/main/java/spark/TemplateViewRoute.java
@@ -33,6 +33,7 @@ public interface TemplateViewRoute {
      * @param request  The request object providing information about the HTTP request
      * @param response The response object providing functionality for modifying the response
      * @return The content to be set in the response
+     * @throws java.lang.Exception
      */
     ModelAndView handle(Request request, Response response) throws Exception;
 
