1d2c766a737e1d24d891349a7a9df5e3b25447ad
Per Wendel <per.i.wendel@gmail.com>
2015-09-30 22:14:07 +0200

    minor changes, added Gzip TODO in matcher filter

--- src/main/java/spark/webserver/MatcherFilter.java
+++ src/main/java/spark/webserver/MatcherFilter.java
@@ -95,6 +95,7 @@ public class MatcherFilter implements Filter {
         boolean consumedByStaticFile = StaticFiles.consume(httpRequest, httpResponse);
 
         if (consumedByStaticFile) {
+            // TODO: GzipUtils.checkAndWrap?
             return;
         }
 
