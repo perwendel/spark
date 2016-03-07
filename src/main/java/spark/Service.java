5755cf96d1689e1eda7dd2d7e03f1eff3bf5a074
Jan-Willem Gmelig Meyling <j.gmeligmeyling@student.tudelft.nl>
2016-03-07 16:14:03 +0100

    Type info for ExceptionHandler

--- src/main/java/spark/Service.java
+++ src/main/java/spark/Service.java
@@ -540,11 +540,11 @@ public final class Service extends Routable {
      * @param exceptionClass the exception class
      * @param handler        The handler
      */
-    public synchronized void exception(Class<? extends Exception> exceptionClass, ExceptionHandler handler) {
+    public synchronized <T extends Exception> void exception(Class<T> exceptionClass, ExceptionHandler<? super T> handler) {
         // wrap
-        ExceptionHandlerImpl wrapper = new ExceptionHandlerImpl(exceptionClass) {
+        ExceptionHandlerImpl wrapper = new ExceptionHandlerImpl<T>(exceptionClass) {
             @Override
-            public void handle(Exception exception, Request request, Response response) {
+            public void handle(T exception, Request request, Response response) {
                 handler.handle(exception, request, response);
             }
         };
