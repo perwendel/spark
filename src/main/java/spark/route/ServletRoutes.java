df5887be1099fca3992f45848b2acbad46343435
Toshiaki Kameyama <kmymtsak@gmail.com>
2016-04-01 16:45:08 +0900

    Remove unused field

--- src/main/java/spark/route/ServletRoutes.java
+++ src/main/java/spark/route/ServletRoutes.java
@@ -4,7 +4,7 @@
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- *  
+ *
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -23,7 +23,6 @@ package spark.route;
  * @author Per Wendel
  */
 public final class ServletRoutes {
-    private static final org.slf4j.Logger LOG = org.slf4j.LoggerFactory.getLogger(ServletRoutes.class);
 
     private static Routes routes = null;
 
