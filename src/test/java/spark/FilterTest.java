4a6a3888d89c0070268d827d060f660209bdc07b
Per Wendel <per.i.wendel@gmail.com>
2016-09-01 21:50:24 +0200

    trying to fix test

--- src/test/java/spark/FilterTest.java
+++ src/test/java/spark/FilterTest.java
@@ -10,14 +10,16 @@ import org.junit.Test;
 import spark.util.SparkTestUtil;
 import spark.util.SparkTestUtil.UrlResponse;
 
+import static spark.Spark.awaitInitialization;
 import static spark.Spark.before;
+import static spark.Spark.stop;
 
 public class FilterTest {
     static SparkTestUtil testUtil;
 
     @AfterClass
     public static void tearDown() {
-        Spark.stop();
+        stop();
     }
 
     @BeforeClass
@@ -25,11 +27,14 @@ public class FilterTest {
         testUtil = new SparkTestUtil(4567);
 
         before("/justfilter", (q, a) -> System.out.println("Filter matched"));
+        awaitInitialization();
     }
 
     @Test
     public void testJustFilter() throws Exception {
         UrlResponse response = testUtil.doMethod("GET", "/justfilter", null);
+
+        System.out.println("response.status = " + response.status);
         Assert.assertEquals(404, response.status);
     }
 
