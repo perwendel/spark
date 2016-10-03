579b7be5598394210337a207557a34c6c4421ebe
Jani Kaarela <jani.kaarela@gmail.com>
2016-10-03 21:16:58 +0200

    Deleted redundant LICENSE file, formatting fixes (tabs -> spaces).

--- core/src/main/java/spark/websocket/WebSocketHandlerWrapper.java
+++ core/src/main/java/spark/websocket/WebSocketHandlerWrapper.java
@@ -4,14 +4,14 @@ package spark.websocket;
  * A wrapper for web socket handler classes/instances.
  */
 public interface WebSocketHandlerWrapper {
-    
-	/**
-	 * Gets the handler class.
-	 * 
-	 * @return The handler class.
-	 */
-	Class<?> getHandlerClass();
-	
+
+    /**
+     * Gets the handler class.
+     * 
+     * @return The handler class.
+     */
+    Class<?> getHandlerClass();
+
     /**
      * Gets the actual handler - if necessary, instantiating an object.
      * 
