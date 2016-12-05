1a08a6b139a9d884105249d976f2bf363377dffb
fdumoulin <fdumoulin@towerlogic.com>
2016-12-05 12:14:48 -0500

    Fix for issues 722 and 723 +comments.

--- src/main/java/spark/CustomErrorPages.java
+++ src/main/java/spark/CustomErrorPages.java
@@ -27,20 +27,20 @@ import org.slf4j.LoggerFactory;
  * be shared between different instances of the Service class.
  */
 public class CustomErrorPages {
-	
-	private static final Logger LOG = LoggerFactory.getLogger(CustomErrorPages.class);
+
+    private static final Logger LOG = LoggerFactory.getLogger(CustomErrorPages.class);
     public static final String NOT_FOUND = "<html><body><h2>404 Not found</h2></body></html>";
     public static final String INTERNAL_ERROR = "<html><body><h2>500 Internal Server Error</h2></body></html>";
-    
+
     /**
      * Verifies that a custom error page exists for the given status code
      * @param status
      * @return true if error page exists
      */
     public static boolean existsFor(int status) {
-    	return CustomErrorPages.getInstance().customPages.containsKey(status);
+        return CustomErrorPages.getInstance().customPages.containsKey(status);
     }
-    
+
     /**
      * Gets the custom error page for a given status code.  If the custom
      * error page is a route, the output of its handle method is returned.
@@ -51,27 +51,24 @@ public class CustomErrorPages {
      * @return Object representing the custom error page
      */
     public static Object getFor(int status, Request request, Response response) {
-    	
+
         Object customRenderer = CustomErrorPages.getInstance().customPages.get(status);
         Object customPage = CustomErrorPages.getInstance().getDefaultFor(status);
-        
+
         if (customRenderer instanceof String) {
-        	
-        	customPage = customRenderer;
-        	
+            customPage = customRenderer;
         } else if (customRenderer instanceof Route) {
-        	
             try {
-            	customPage = ((Route) customRenderer).handle(request, response);
+                customPage = ((Route) customRenderer).handle(request, response);
             } catch (Exception e) {
-            	// The custom page renderer is causing an internal server error.  Log exception as a warning and use default page instead
-            	LOG.warn("Custom error page handler for status code {} has thrown an exception: {}. Using default page instead.", status, e.getMessage());
+             // The custom page renderer is causing an internal server error.  Log exception as a warning and use default page instead
+                LOG.warn("Custom error page handler for status code {} has thrown an exception: {}. Using default page instead.", status, e.getMessage());
             }
-        } 
-        
-        return customPage;        
+        }
+
+        return customPage;
     }
-    
+
     /**
      * Returns the default error page for a given status code.
      * Guaranteed to never be null.
@@ -79,8 +76,8 @@ public class CustomErrorPages {
      * @return String representation of the default error page.
      */
     public String getDefaultFor(int status){
-    	String defaultPage = defaultPages.get(status);
-    	return (defaultPage != null) ? defaultPage : "<html><body><h2>HTTP Status " + status + "</h2></body></html>";
+        String defaultPage = defaultPages.get(status);
+        return (defaultPage != null) ? defaultPage : "<html><body><h2>HTTP Status " + status + "</h2></body></html>";
     }
     
     /**
@@ -91,7 +88,7 @@ public class CustomErrorPages {
     static void add(int status, String page) {
         CustomErrorPages.getInstance().customPages.put(status, page);
     }
-    
+
     /**
      * Add a custom error page as a Route handler
      * @param status
@@ -102,6 +99,7 @@ public class CustomErrorPages {
     }
 
     // Private stuff
+
     private final HashMap<Integer, Object> customPages;
     private final HashMap<Integer, String> defaultPages;
 
