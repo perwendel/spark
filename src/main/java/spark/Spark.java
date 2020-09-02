ea1b77c1c28959ebe116e16705abf2ecc3647c24
dredmond <dustinkredmond@gmail.com>
2020-09-02 15:25:36 -0400

    Add methods for removing routes at runtime

--- src/main/java/spark/Spark.java
+++ src/main/java/spark/Spark.java
@@ -864,6 +864,33 @@ public class Spark {
         getInstance().patch(path, acceptType, route, transformer);
     }
 
+    /**
+     * Removes a particular route from the collection of those that have been previously routed.
+     * Search for previously established routes using the given path and removes any matches that are found.
+     *
+     * @param path          the route path
+     * @return              <tt>true</tt> if this is a matching route which has been previously routed
+     * @throws IllegalArgumentException if <tt>path</tt> is null or blank
+     */
+    public static boolean removeRoute(String path) {
+        return getInstance().routes.remove(path);
+    }
+
+    /**
+     * Removes a particular route from the collection of those that have been previously routed.
+     * Search for previously established routes using the given path and HTTP method, removing any
+     * matches that are found.
+     *
+     * @param path          the route path
+     * @param httpMethod    the http method
+     * @return <tt>true</tt> if this is a matching route that has been previously routed
+     * @throws IllegalArgumentException if <tt>path</tt> is null or blank or if <tt>httpMethod</tt> is null, blank,
+     * or an invalid HTTP method
+     */
+    public static boolean removeRoute(String path, String httpMethod) {
+        return getInstance().routes.remove(path, httpMethod);
+    }
+
     //////////////////////////////////////////////////
     // END Response Transforming Routes
     //////////////////////////////////////////////////
